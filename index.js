f1964de1e06f2e4a303364bbbdd666d1c1e036d7
SoCuul <63339559+SoCuul@users.noreply.github.com>
2021-07-23 18:08:27 -0700

    Add RBXBolt support + Updates
    - Added RBXBolt support
    - Improved consistency in the code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 const axios = require('axios');
 
 //List of verification services
-module.exports.services = ['bloxlink', 'rover', 'rowifi']
+module.exports.services = ['bloxlink', 'rover', 'rowifi', 'rbxbolt']
 
 //Bloxlink
 async function bloxlink(discordid, guildid) {
@@ -35,29 +35,29 @@ async function bloxlink(discordid, guildid) {
         throw new TypeError('The provided guild ID must only contain valid positive integers')
     }
 
-    //Prepare for bloxlinkError
-    let bloxlinkError = null
+    //Prepare for verifyError
+    let verifyError = null
 
     try{
         //Make bloxlink request
-        const bloxlinkRequest = await axios.get('https://api.blox.link/v1/user/' + discordid, {
+        const verifyRequest = await axios.get('https://api.blox.link/v1/user/' + discordid, {
             params: {
                 guild: guildid
             }
         })
 
         //Check for bloxlink errors
-        if(bloxlinkRequest.data && bloxlinkRequest.data.status && bloxlinkRequest.data.status === 'error'){
-            bloxlinkError = {
-                status: bloxlinkRequest.status,
-                data: bloxlinkRequest.data
+        if(verifyRequest.data && verifyRequest.data.status && verifyRequest.data.status === 'error'){
+            verifyError = {
+                status: verifyRequest.status,
+                data: verifyRequest.data
             }
 
-            throw new Error('bloxlinkError')
+            throw new Error('verifyError')
         }
         
         //Make roblox request
-        const robloxRequest = await axios.get(`https://users.roblox.com/v1/users/${bloxlinkRequest.data.matchingAccount ? bloxlinkRequest.data.matchingAccount : bloxlinkRequest.data.primaryAccount}`)
+        const robloxRequest = await axios.get(`https://users.roblox.com/v1/users/${verifyRequest.data.matchingAccount ? verifyRequest.data.matchingAccount : verifyRequest.data.primaryAccount}`)
 
         let response = {
             "status": "success"
@@ -68,7 +68,7 @@ async function bloxlink(discordid, guildid) {
             response.robloxID = robloxRequest.data.id
         }
         else{
-            response.robloxID = bloxlinkRequest.data.matchingAccount || bloxlinkRequest.data.primaryAccount || null
+            response.robloxID = verifyRequest.data.matchingAccount || verifyRequest.data.primaryAccount || null
         }
 
         //Set roblox username
@@ -88,8 +88,8 @@ async function bloxlink(discordid, guildid) {
         }
 
         //Set error code
-        if(bloxlinkError && bloxlinkError.status){
-            errorResponse.errorCode = bloxlinkError.status
+        if(verifyError && verifyError.status){
+            errorResponse.errorCode = verifyError.status
         }
         else if(error.response && error.response.status){
             errorResponse.errorCode = error.response.status
@@ -99,7 +99,7 @@ async function bloxlink(discordid, guildid) {
         }
 
         //Set error type
-        if(bloxlinkError && bloxlinkError.data && bloxlinkError.data.status && bloxlinkError.data.status === 'error'){
+        if(verifyError && verifyError.data && verifyError.data.status && verifyError.data.status === 'error'){
             errorResponse.errorType = 'bloxlink'
         } 
         else if(error.response && error.response.data && error.response.data.errors && error.response.data.errors[0]){
@@ -111,13 +111,13 @@ async function bloxlink(discordid, guildid) {
 
         //Set error
         if(errorResponse.errorType === 'bloxlink'){
-            if(bloxlinkError.data.error){
-                errorResponse.error = bloxlinkError.data.error
-                errorResponse.rawError = bloxlinkError.data
+            if(verifyError.data.error){
+                errorResponse.error = verifyError.data.error
+                errorResponse.rawError = verifyError.data
             }
             else{
                 errorResponse.error = 'View rawError'
-                errorResponse.rawError = bloxlinkError.data
+                errorResponse.rawError = verifyError.data
             }
         }
         else if(errorResponse.errorType === 'roblox'){
@@ -163,23 +163,23 @@ async function rover(discordid) {
 
     try{
         //Make RoVer request
-        const request = await axios.get('https://verify.eryn.io/api/user/' + discordid)
+        const verifyRequest = await axios.get('https://verify.eryn.io/api/user/' + discordid)
         
         let response = {
             "status": "success"
         }
 
         //Set roblox id
-        if(request.data && request.data.robloxId){
-            response.robloxID = request.data.robloxId
+        if(verifyRequest.data && verifyRequest.data.robloxId){
+            response.robloxID = verifyRequest.data.robloxId
         }
         else{
             response.robloxID = null
         }
 
         //Set roblox username
-        if(request.data && request.data.robloxUsername){
-            response.robloxUsername = request.data.robloxUsername
+        if(verifyRequest.data && verifyRequest.data.robloxUsername){
+            response.robloxUsername = verifyRequest.data.robloxUsername
         }
         else{
             response.robloxUsername = null
@@ -262,25 +262,25 @@ async function rowifi(discordid, guildid) {
         throw new TypeError('The provided guild ID must only contain valid positive integers')
     }
 
-    //Prepare for rowifiError
-    let rowifiError = null
+    //Prepare for verifyError
+    let verifyError = null
 
     try{
         //Make rowifi request
-        const rowifiRequest = await axios.get('https://api.rowifi.link/v1/users/' + discordid + `${guildid ? `?guildid=${guildid}` : ''}`)
+        const verifyRequest = await axios.get('https://api.rowifi.link/v1/users/' + discordid + `${guildid ? `?guildid=${guildid}` : ''}`)
 
         //Check for rowifi errors
-        if(rowifiRequest.data && typeof rowifiRequest.data.success !== 'undefined' && rowifiRequest.data.success === false){
-            rowifiError = {
-                status: rowifiRequest.status,
-                data: rowifiRequest.data
+        if(verifyRequest.data && typeof verifyRequest.data.success !== 'undefined' && verifyRequest.data.success === false){
+            verifyError = {
+                status: verifyRequest.status,
+                data: verifyRequest.data
             }
 
-            throw new Error('rowifiError')
+            throw new Error('verifyError')
         }
         
         //Make roblox request
-        const robloxRequest = await axios.get(`https://users.roblox.com/v1/users/${rowifiRequest.data.roblox_id}`)
+        const robloxRequest = await axios.get(`https://users.roblox.com/v1/users/${verifyRequest.data.roblox_id}`)
 
         let response = {
             "status": "success"
@@ -291,7 +291,7 @@ async function rowifi(discordid, guildid) {
             response.robloxID = robloxRequest.data.id
         }
         else{
-            response.robloxID = rowifiRequest.data.roblox_id || null
+            response.robloxID = verifyRequest.data.roblox_id || null
         }
 
         //Set roblox username
@@ -311,8 +311,8 @@ async function rowifi(discordid, guildid) {
         }
 
         //Set error code
-        if(rowifiError && rowifiError.status){
-            errorResponse.errorCode = rowifiError.status
+        if(verifyError && verifyError.status){
+            errorResponse.errorCode = verifyError.status
         }
         else if(error.response && error.response.status){
             errorResponse.errorCode = error.response.status
@@ -322,7 +322,7 @@ async function rowifi(discordid, guildid) {
         }
 
         //Set error type
-        if(rowifiError && rowifiError.data &&  typeof rowifiError.data.success !== 'undefined' && rowifiError.data.success === false){
+        if(verifyError && verifyError.data &&  typeof verifyError.data.success !== 'undefined' && verifyError.data.success === false){
             errorResponse.errorType = 'rowifi'
         }
         else if(error.response && error.response.data && error.response.data.errors && error.response.data.errors[0]){
@@ -334,13 +334,13 @@ async function rowifi(discordid, guildid) {
 
         //Set error
         if(errorResponse.errorType === 'rowifi'){
-            if(rowifiError.data.message){
-                errorResponse.error = rowifiError.data.message
-                errorResponse.rawError = rowifiError.data
+            if(verifyError.data.message){
+                errorResponse.error = verifyError.data.message
+                errorResponse.rawError = verifyError.data
             }
             else{
                 errorResponse.error = 'View rawError'
-                errorResponse.rawError = rowifiError.data
+                errorResponse.rawError = verifyError.data
             }
         }
         else if(errorResponse.errorType === 'roblox'){
@@ -366,7 +366,120 @@ async function rowifi(discordid, guildid) {
     }
 }
 
+//RBXBolt
+async function rbxbolt(discordid, key) {
+    //Check for variables
+    if(!discordid) throw new Error('No Discord ID was provided')
+    if(!key) throw new Error('No API key was provided')
+
+    //Check for variable types
+    if(typeof discordid !== "string") throw new TypeError('The provided Discord ID must be a string')
+    if(typeof key !== "string") throw new TypeError('The provided API Key must be a string')
+
+    //Check for variable validity
+    //Discord ID
+    try{
+        let int = BigInt(discordid)
+        if(int < 0) throw new Error("Negative")
+    }
+    catch(error){
+        throw new TypeError('The provided Discord ID must only contain valid positive integers')
+    }
+
+    //Prepare for verifyError
+    let verifyError = null
+
+    try{
+        //Make rbxbolt request
+        const verifyRequest = await axios.get('https://api.rbxbolt.com/v1/discord/' + discordid + `?key=${key}`)
+
+        //Check for rbxbolt errors
+        if(verifyRequest.data && typeof verifyRequest.data.success !== 'undefined' && verifyRequest.data.success === false){
+            verifyError = {
+                status: verifyRequest.status,
+                data: verifyRequest.data
+            }
+
+            throw new Error('verifyError')
+        }
+
+        let response = {
+            "status": "success"
+        }
+
+        //Set roblox id
+        if(verifyRequest.data && verifyRequest.data.UserId && Number(verifyRequest.data.UserId)){
+            response.robloxID = Number(verifyRequest.data.UserId)
+        }
+        else{
+            response.robloxID = null
+        }
+
+        //Set roblox username
+        if(verifyRequest.data && verifyRequest.data.Username){
+            response.robloxUsername = verifyRequest.data.Username
+        }
+        else{
+            response.robloxUsername = null
+        }
+
+        //Return information
+        return response
+    }
+    catch(error){
+        let errorResponse = {
+            "status": "error"
+        }
+
+        //Set error code
+        if(verifyError && verifyError.status){
+            errorResponse.errorCode = verifyError.status
+        }
+        else if(error.response && error.response.status){
+            errorResponse.errorCode = error.response.status
+        }
+        else{
+            errorResponse.errorCode = null
+        }
+
+        //Set error type
+        if(verifyError && verifyError.data &&  typeof verifyError.data.success !== 'undefined' && verifyError.data.success === false){
+            errorResponse.errorType = 'rbxbolt'
+        }
+        else{
+            errorResponse.errorType = 'node'
+        }
+
+        //Set error
+        if(errorResponse.errorType === 'rbxbolt'){
+            if(verifyError.data.message){
+                errorResponse.error = verifyError.data.message
+                errorResponse.rawError = verifyError.data
+            }
+            else if(verifyError.data.error){
+                errorResponse.error = verifyError.data.error
+                errorResponse.rawError = verifyError.data
+            }
+            else{
+                errorResponse.error = 'View rawError'
+                errorResponse.rawError = verifyError.data
+            }
+        }
+        else if(errorResponse.errorType === 'node'){
+            errorResponse.error = error
+            errorResponse.rawError = null
+        }
+        else{
+            errorResponse.error = null
+        }
+
+        //Return error info
+        return errorResponse
+    }
+}
+
 //Export functions
 module.exports.bloxlink = bloxlink
 module.exports.rover = rover
 module.exports.rowifi = rowifi
+module.exports.rbxbolt = rbxbolt
